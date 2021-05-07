73439ea4ebcedebdab1a4c9b032506f0ef5335b2
Sarah Drasner <sarah.drasner@gmail.com>
2021-05-07 11:56:39 -0600

    get some content in there

--- content/recipes/first-recipe.md
+++ content/recipes/first-recipe.md
@@ -1,3 +1,10 @@
-# First recipe
+---
+title: First Recipe
+description: Doin something
+img: cucumbersoup.png
+alt: pretty greenery
+---
+
+## This is a section
 
 Trying this out.
--- pages/recipes/_slug.vue
+++ pages/recipes/_slug.vue
@@ -1,7 +1,12 @@
 <template>
-  <recipe>
+  <article>
+    <h1>{{ recipe.title }}</h1>
+    <p>{{ recipe.description }}</p>
+    <img :src="`../${recipe.img}`" :alt="recipe.alt" class="recipe-img" />
+    <p>Article last updated: {{ formatDate(recipe.updatedAt) }}</p>
+
     <nuxt-content :document="recipe" />
-  </recipe>
+  </article>
 </template>
 
 <script>
@@ -10,6 +15,18 @@ export default {
     const recipe = await $content("recipes", params.slug).fetch();
 
     return { recipe };
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Date(date).toLocaleDateString("en", options);
+    }
   }
 };
 </script>
+
+<style scoped>
+.recipe-img {
+  width: 300px;
+}
+</style>
