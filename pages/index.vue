e89dbe4273076933b89ed6f888e19a53cbd87f43
Sarah Drasner <sarah.drasner@gmail.com>
2021-05-07 13:03:24 -0600

    homepage and cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <main>
-    <h1>Hi there</h1>
-    <p>Heya gator</p>
+    <h1>Recipes</h1>
+    <section v-for="recipe of recipes" :key="recipe.slug">
+      <NuxtLink :to="{ name: 'blog-slug', params: { slug: recipe.slug } }">
+        <img :src="recipe.img" class="recipe-list-img" />
+        <div>
+          <h2>{{ recipe.title }}</h2>
+          <p>by {{ recipe.author.name }}</p>
+          <p>{{ recipe.description }}</p>
+        </div>
+      </NuxtLink>
+    </section>
   </main>
 </template>
 
@@ -19,4 +28,9 @@ export default {
   }
 };
 </script>
-<style></style>
+
+<style>
+.recipe-list-img {
+  width: 200px;
+}
+</style>
